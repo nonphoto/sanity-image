04f7d1415d4a3b4b021fc9465587031fb598d317
Jonas Luebbers <jonas@jonasluebbers.com>
2023-03-08 15:58:09 -0800

    Initial commit

---
+++ vite.config.ts.js
@@ -0,0 +1,15 @@
+// vite.config.ts
+import { defineConfig } from "vite";
+var vite_config_default = defineConfig({
+  build: {
+    lib: {
+      entry: "./lib/main.ts",
+      name: "Counter",
+      fileName: "counter"
+    }
+  }
+});
+export {
+  vite_config_default as default
+};
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsidml0ZS5jb25maWcudHMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbImltcG9ydCB7IGRlZmluZUNvbmZpZyB9IGZyb20gJ3ZpdGUnXG5cbmV4cG9ydCBkZWZhdWx0IGRlZmluZUNvbmZpZyh7XG4gIGJ1aWxkOiB7XG4gICAgbGliOiB7XG4gICAgICBlbnRyeTogJy4vbGliL21haW4udHMnLFxuICAgICAgbmFtZTogJ0NvdW50ZXInLFxuICAgICAgZmlsZU5hbWU6ICdjb3VudGVyJ1xuICAgIH1cbiAgfVxufSlcbiJdLAogICJtYXBwaW5ncyI6ICI7QUFBQTtBQUVBLElBQU8sc0JBQVEsYUFBYTtBQUFBLEVBQzFCLE9BQU87QUFBQSxJQUNMLEtBQUs7QUFBQSxNQUNILE9BQU87QUFBQSxNQUNQLE1BQU07QUFBQSxNQUNOLFVBQVU7QUFBQSxJQUNaO0FBQUEsRUFDRjtBQUNGLENBQUM7IiwKICAibmFtZXMiOiBbXQp9Cg==
