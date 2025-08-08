8901f6ba070e58b4651fde60d2f76aa5964ff36a
Jonas Luebbers <jonas@jonasluebbers.com>
2025-08-08 10:39:58 -0400

    Add croppedSize, imageCroppedSize, aspectRatio, imageAspectRatio

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import imageUrlBuilder from "@sanity/image-url";
 import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
 import type {
+  SanityAsset,
   SanityClientLike,
   SanityImageCrop,
   SanityImageDimensions,
+  SanityImageHotspot,
+  SanityImageObject,
   SanityImageSource,
   SanityModernClientLike,
   SanityProjectDetails,
+  SanityReference,
 } from "@sanity/image-url/lib/types/types.js";
 
-export interface Metadata {
+export interface SanityImageObjectLike
+  extends Pick<SanityImageObject, "asset"> {
+  [key: string]: any;
+}
+
+export interface SanityImageMetadata {
   lqip?: string;
   dimensions?: SanityImageDimensions;
+  [key: string]: any;
 }
 
 export interface Size {
@@ -45,13 +55,13 @@ export const defaultMetaImageWidth = 1200;
 
 export const defaultQuality = 90;
 
-const fitComparators = {
+export const fitComparators = {
   cover: Math.max,
   contain: Math.min,
-  mean: (a: number, b: number) => (a + b) / 2,
 };
 
-function fit(
+// TODO: impement comparators for every FitMode
+export function fit(
   containee: Size,
   container: Size,
   mode: keyof typeof fitComparators
@@ -77,9 +87,9 @@ function buildAspectRatio(
   }
 }
 
-function isCrop(x: any): x is SanityImageCrop {
+export function isCrop(x: unknown): x is SanityImageCrop {
   return (
-    x &&
+    x != null &&
     typeof x == "object" &&
     "top" in x &&
     typeof x.top === "number" &&
@@ -88,14 +98,106 @@ function isCrop(x: any): x is SanityImageCrop {
     "bottom" in x &&
     typeof x.bottom === "number" &&
     "left" in x &&
-    typeof x.left === "number"
+    typeof x.left === "number" &&
+    ("_type" in x
+      ? typeof x._type === "undefined" || typeof x._type === "string"
+      : true)
+  );
+}
+
+export function isHotspot(x: unknown): x is SanityImageHotspot {
+  return (
+    x != null &&
+    typeof x == "object" &&
+    "width" in x &&
+    typeof x.width === "number" &&
+    "height" in x &&
+    typeof x.height === "number" &&
+    "x" in x &&
+    typeof x.x === "number" &&
+    "y" in x &&
+    typeof x.y === "number" &&
+    ("_type" in x
+      ? typeof x._type === "undefined" || typeof x._type === "string"
+      : true)
+  );
+}
+
+export function isImageObjectLike(x: unknown): x is SanityImageObjectLike {
+  return (
+    x != null &&
+    typeof x === "object" &&
+    "asset" in x &&
+    (isAsset(x) || isReference(x))
+  );
+}
+
+export function isImageObject(x: unknown): x is SanityImageObject {
+  return (
+    isImageObjectLike(x) &&
+    ("crop" in x ? typeof x.crop === "undefined" || isCrop(x.crop) : true) &&
+    ("hotspot" in x
+      ? typeof x.hotspot === "undefined" || isHotspot(x.hotspot)
+      : true)
+  );
+}
+
+export function isReference(x: unknown): x is SanityReference {
+  return (
+    x != null &&
+    typeof x === "object" &&
+    "_ref" in x &&
+    typeof x._ref === "string"
+  );
+}
+
+export function isAsset(x: unknown): x is SanityAsset {
+  return (
+    x != null &&
+    typeof x === "object" &&
+    "_type" in x &&
+    x._type === "sanity.asset"
+  );
+}
+
+export function isDimensions(x: unknown): x is SanityImageDimensions {
+  return (
+    x != null &&
+    typeof x === "object" &&
+    "aspectRatio" in x &&
+    typeof x.aspectRatio === "number" &&
+    "width" in x &&
+    typeof x.width === "number" &&
+    "height" in x &&
+    typeof x.height === "number"
+  );
+}
+
+export function isSize(x: unknown): x is Size {
+  return (
+    x != null &&
+    typeof x === "object" &&
+    "width" in x &&
+    typeof x.width === "number" &&
+    "height" in x &&
+    typeof x.height === "number"
+  );
+}
+
+export function isMetadata(x: unknown): x is SanityImageMetadata {
+  return (
+    x != null &&
+    typeof x === "object" &&
+    "lqip" in x &&
+    typeof x.lqip === "string" &&
+    "dimensions" in x &&
+    isDimensions(x.dimensions)
   );
 }
 
 export interface ImagePropsOptions {
   client: SanityClientLike | SanityProjectDetails | SanityModernClientLike;
-  image: SanityImageSource;
-  metadata?: Metadata;
+  source: SanityImageSource;
   widths?: number[];
   quality?: number;
   aspectRatio?: number;
@@ -104,53 +206,70 @@ export interface ImagePropsOptions {
 export interface ImagePropsReturn {
   src: string;
   srcset?: string;
-  naturalWidth?: number;
-  naturalHeight?: number;
 }
 
 export function imageProps({
   client,
-  image,
-  metadata,
-  widths,
+  source,
+  widths = defaultWidths,
   quality = defaultQuality,
   aspectRatio,
 }: ImagePropsOptions): ImagePropsReturn {
   const builder = imageUrlBuilder(client)
-    .image(image)
+    .image(source)
     .quality(quality)
     .auto("format");
-  const crop =
-    typeof image == "object" && "crop" in image && isCrop(image.crop)
-      ? image.crop
-      : undefined;
-  const cropSize = metadata?.dimensions
-    ? crop
-      ? {
-          width: metadata.dimensions.width - crop.left - crop.right,
-          height: metadata.dimensions.height - crop.top - crop.bottom,
-        }
-      : metadata.dimensions
-    : undefined;
-
-  const naturalSize = cropSize
-    ? aspectRatio
-      ? fit({ width: 1, height: aspectRatio }, cropSize, "contain")
-      : cropSize
-    : undefined;
   const url = buildAspectRatio(builder, lowResWidth, aspectRatio).url();
   return {
-    src: widths ? url : metadata?.lqip ?? url,
-    srcset: widths
-      ? Array.from(widths)
-          .sort((a, b) => a - b)
-          .map(
-            (width) =>
-              `${buildAspectRatio(builder, width, aspectRatio).url()} ${width}w`
-          )
-          .join(",")
-      : undefined,
-    naturalWidth: naturalSize?.width,
-    naturalHeight: naturalSize?.height,
+    src: url,
+    srcset:
+      widths.length > 0
+        ? widths
+            .sort((a, b) => a - b)
+            .map(
+              (width) =>
+                `${buildAspectRatio(
+                  builder,
+                  width,
+                  aspectRatio
+                ).url()} ${width}w`
+            )
+            .join(",")
+        : undefined,
   };
+}
+
+export function croppedSize(intrinsicSize: Size, crop: SanityImageCrop): Size {
+  return {
+    width: intrinsicSize.width - crop.left - crop.right,
+    height: intrinsicSize.height - crop.top - crop.bottom,
+  };
+}
+
+export function aspectRatio(size: Size): number {
+  return size.height / size.width;
+}
+
+export function imageCroppedSize(source: SanityImageSource): Size | undefined {
+  const imageObject = isImageObject(source) ? source : undefined;
+  const dimensions =
+    imageObject &&
+    isAsset(imageObject.asset) &&
+    isDimensions(imageObject.asset.dimensions)
+      ? imageObject.asset.dimensions
+      : isAsset(source) && isDimensions(source.dimensions)
+      ? source.dimensions
+      : undefined;
+  return dimensions
+    ? imageObject?.crop
+      ? croppedSize(dimensions, imageObject.crop)
+      : dimensions
+    : undefined;
+}
+
+export function imageAspectRatio(
+  source: SanityImageSource
+): number | undefined {
+  const size = imageCroppedSize(source);
+  return size ? aspectRatio(size) : undefined;
 }
